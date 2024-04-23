378d6d6b0b5df8f48e8f8f209cc9effda70527c4
TharushaKula <119743996+TharushaKula@users.noreply.github.com>
2024-04-23 13:21:01 +0530

    Alignments added

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,10 +9,10 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
+        <li>Java</li>
+        <li>Flutter</li>
+        <li>Python</li>
+        <li>R</li>
         <li>JavaScript</li>
         <li>React</li>
       </ul>
@@ -23,8 +23,8 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li>S. Thomas' College, Mount Lavinia</li>
+        <li>University of Informatics Institute of Technology</li>
       </ul>
     ),
   },
@@ -33,8 +33,8 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li> </li>
+        <li> </li>
       </ul>
     ),
   },
@@ -57,12 +57,15 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+          I am an enthusiastic, hardworking
+          individual who is now looking for
+          an opportunity to build my career,
+          to reach my goals in life. Being a
+          team player, I want to follow field
+          that suits and interests me to get to
+          my aims and to contribute
+          remarkably in whatever I do.
+
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
